f88a5c87b7efbc4c49d0aff0c18e510917a36d8c
J180481 <J180481@tafe.wa.edu.au>
2026-06-08 21:28:17 +0800

    chore: refactored catfact namespace

--- CatFactApp/Models/CatFact.cs
+++ CatFactApp/Models/CatFact.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CatFactApp
+namespace CatFactApp.Models
 {
     public class CatFact
     {
--- CatFactApp/Services/SQLService.cs
+++ CatFactApp/Services/SQLService.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CatFactApp.Models;
 
-namespace CatFactApp
+namespace CatFactApp.Services
 {
     public class SQLService
     {
