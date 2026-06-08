6496f5d57eec078d88543d85a6ff65dfc8f7838d
J180481 <J180481@tafe.wa.edu.au>
2026-06-08 21:31:25 +0800

    Refactor: added control code to MainController
    took code from old mainpage.cs and refactored into controller tasks

--- CatFactApp/Controllers/MainController.cs
+++ CatFactApp/Controllers/MainController.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CatFactApp.Models;
+using CatFactApp.Services;
 
 namespace CatFactApp.Controllers
 {
-    class MainController
+    public class MainController
     {
+        ApiService _apiService = new ApiService();
+        int count = 0;
+        int dogCount = 10;
+
+        public async Task<(string headerText, string factText, string imageSource)> GetFactControl()
+        {
+            count++;
+            if (count == dogCount)
+            {
+                string dogUrl = await _apiService.GetDogURL();
+                count = 0;
+                return ("Dog Time!!!!", "Ruff ruff ruff ruff ruff ruff ruff... woof", dogUrl);
+            }
+
+            CatFact fact = await _apiService.GetFact();
+            return ("Meow Facts!", fact.data[0], "cat.png");
+        }
+
+        public async Task SaveFactControl(string factText)
+        {
+            var fact = new DisplayFact
+            {
+                fact = factText,
+                time = DateTime.Now
+            };
+            await App.DatabaseService.SaveFactAsync(fact);
+        }
+
+        public async Task GoToAboutControl()
+        {
+            await Shell.Current.GoToAsync("AboutPage");
+        }
+
+        public async Task GoToOptionsControl()
+        {
+            await Shell.Current.GoToAsync("OptionsMenuPage");
+        }
+
+        public async Task GoToSavedFactsControl()
+        {
+            await Shell.Current.GoToAsync("SavedFactsPage");
+        }
     }
 }
