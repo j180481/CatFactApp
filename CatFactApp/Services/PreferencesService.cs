8944cf907f61c6846f49135c2b7c60188af69410
J180481 <J180481@tafe.wa.edu.au>
2026-06-08 21:30:41 +0800

    chore: refactored preference service namespace
    changed the namespace for preferences service to .services

--- CatFactApp/Services/PreferencesService.cs
+++ CatFactApp/Services/PreferencesService.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CatFactApp.Models;
 
-namespace CatFactApp
+namespace CatFactApp.Services
 {
     class PreferencesService
     {
