82edfd472787cbac019321324bf474a6e79d6422
J180481 <J180481@tafe.wa.edu.au>
2026-06-10 07:54:50 +0800

    documenttion: explained tapcommand in documentation

--- CatFactApp/Views/AboutPage.xaml.cs
+++ CatFactApp/Views/AboutPage.xaml.cs
@@ -8,6 +8,12 @@ public partial class AboutPage : ContentPage
 
     AboutController controller = new AboutController();
 
+    /// <summary>
+    /// In the xaml, the labels are connected to a gesture tap recognizer.
+    /// The tap recognizer is bound to the tap command, with the api url as the command parameter.
+    /// On being tapped, it passes the url (or the string) parameter into TapCommand which opens the link in the systems default browser.
+    /// Resource: https://learn.microsoft.com/en-us/answers/questions/563910/label-hyperlink
+    /// </summary>
     public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
 
     public AboutPage()
