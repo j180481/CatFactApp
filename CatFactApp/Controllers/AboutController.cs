7a1c05d5f2aed7a8fcf37569006f2f5709ee948f
J180481 <J180481@tafe.wa.edu.au>
2026-06-10 02:22:55 +0800

    Feature: added about page controller
    added about page controller
    
    with functions to retrieve header size from preferences service and body size

---
+++ CatFactApp/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatFactApp.Models;
+using CatFactApp.Services;
+
+namespace CatFactApp.Controllers
+{
+    public class AboutController
+    {
+        public static int GetHeaderSize()
+        {
+            return PreferencesService.HeaderSize;
+        }
+
+        public static int GetBodySize()
+        {
+            return PreferencesService.BodySize;
+        }
+    }
+}
