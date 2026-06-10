5479cbf0543dfc4a6c72d18e9ace31f626a96321
J180481 <J180481@tafe.wa.edu.au>
2026-06-10 08:29:14 +0800

    documentation: altered the tap url link

--- CatFactApp/Views/AboutPage.xaml.cs
+++ CatFactApp/Views/AboutPage.xaml.cs
@@ -12,7 +12,7 @@ public partial class AboutPage : ContentPage
     /// In the xaml, the labels are connected to a gesture tap recognizer.
     /// The tap recognizer is bound to the tap command, with the api url as the command parameter.
     /// On being tapped, it passes the url (or the string) parameter into TapCommand which opens the link in the systems default browser.
-    /// Resource: https://learn.microsoft.com/en-us/answers/questions/563910/label-hyperlink
+    /// Resource: https://learn.microsoft.com/en-us/dotnet/maui/user-interface/controls/label?view=net-maui-10.0#create-a-hyperlink
     /// </summary>
     public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
 
